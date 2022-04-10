f669c760370ac08b6ebce712f9e81367a7719bac
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-04-10 20:03:53 -0300

    Index da tela de login

---
+++ tela-de-login/index.php
@@ -0,0 +1,23 @@
+<html>
+<head> <meta charset="UTF-8" /> </head>
+<body>
+<form method="post" action="logica.php" id="formulario" name="formulario">
+    <fieldset id="field">
+        <label>Usuário :</label>
+        <input type="text" name="usuario" id="usuario" /><br />
+
+        <label>Senha :</label>
+        <input type="password" name="senha" id="senha" /><br />
+
+        <input type="submit" value="Entrar" />
+    </fieldset>
+</form>
+</body>
+</html>
+
+<?php
+
+class index
+{
+
+}
