985411afc8bd13847c54e87eb3888d3ef5063f12
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-04-10 20:03:30 -0300

    Atividades

---
+++ ex/prog1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Prog 1</title>
+</head>
+<body>
+<?php
+echo "<center> <h1> Meu primeiro código php! </h1> </center>"
+?>
+</body>
+</html>
---
+++ ex/prog2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Prog 2</title>
+</head>
+<body>
+<?php
+$dia = "25";
+$mes = "Dezembro";
+?>
+<p> O natal é no dia <?php echo $dia; ?> de <?php echo $mes; ?>. </p>
+<?php
+echo `dir`;
+?>
+</body>
+</html>
